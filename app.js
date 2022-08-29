c2b92645e05bc137d255b5b44fd11bdc05879264
Patricio Maldonado <patriciomaldonado@patriciacstudio.lan>
2022-08-29 20:49:13 -0300

    Heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{
     console.log('Servidor corriendo');
 })
 
