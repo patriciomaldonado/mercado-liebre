d0fbb7667b900b10a14b4b1003f9db4c9096cf39
Patricio Maldonado <63500870+patriciomaldonado@users.noreply.github.com>
2022-09-23 15:08:39 -0300

    algo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(process.env.PORT || 3000, () =>{
     console.log('Servidor corriendo');
-})
+})  
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'));
